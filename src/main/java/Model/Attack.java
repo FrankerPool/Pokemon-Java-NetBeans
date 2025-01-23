ae7b23124fc63542b5d3005989ea81b2e67f363d
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2025-01-23 16:40:39 -0600

    Add: template and class

---
+++ src/main/java/Model/Attack.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.awt.font.NumericShaper.Range;
+
+/**
+ *
+ * @author thela
+ */
+public class Attack {
+    protected String Name;
+    protected int Power;
+    protected int Effectiveness;
+}
---
+++ src/main/java/Model/Pokemon.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author thela
+ */
+public class Pokemon {
+    protected String TypePokemon;
+    protected int LevelPokemon;
+    protected int LifePokemon;
+    public List<Attack> myList = new ArrayList<>();
+}
